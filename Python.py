f0566afa7232a4129ce2f15766cf9b980ef763fa
SarfrazAHd <44200619+SarfrazAHd@users.noreply.github.com>
2018-10-16 18:57:15 +0530

    Creating Function

--- Python.py
+++ Python.py
@@ -1,3 +1,9 @@
 print("Hello Users!!")
 
 #this is the first step for me with github.
+
+
+#how to Create a function name in python:
+
+def functionname(arguments):
+  #your code will place here inside of this function.
