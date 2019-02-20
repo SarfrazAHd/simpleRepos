8b3fb56bba0c8468b32aa676d1a9f20b28744b4c
SarfrazAHd <21sarfrazansari@gmail.com>
2019-02-20 22:31:42 +0530

    Commiting A simpl

---
+++ Python_Project/Calculator.py
@@ -0,0 +1,101 @@
+from tkinter import *
+
+
+def btnClick(number):
+    global operator
+    operator = operator + str(number)
+    text_input.set(operator)
+
+
+def btnClear():
+    global operator
+    operator = ""
+    text_input.set((""))
+
+
+def btnEqual():
+    global operator
+    sumup = str(eval(operator))
+    text_input.set(sumup)
+    operator = " "
+
+
+root = Tk()
+root.title("Calculator")
+root.resizable(False, False)
+
+operator = ""
+text_input = StringVar()
+
+inpt = Entry(root, font=('arial', 20, 'bold'), textvariable=text_input, bd=30, insertwidth=4, justify="right",
+             bg="powder blue", fg='blue')
+inpt.grid(columnspan=4)
+
+# 0 . AC -
+
+b0 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="0", bd=6, command=lambda: btnClick(0))
+b0.grid(row=1, column=0)
+
+bdot = Button(root, padx=10, pady=10, font=('times', 18, 'bold'), fg='black', text=".", bd=6,
+              command=lambda: btnClick('.'))
+bdot.grid(row=1, column=1)
+
+b_clear = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="C", bd=6, command=btnClear)
+b_clear.grid(row=1, column=2)
+
+b_minus = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='white', bg='orange', text="-", bd=6,
+                 command=lambda: btnClick('-'))
+b_minus.grid(row=1, column=3)
+
+# 7 8 9 +
+
+b7 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="7", bd=6, command=lambda: btnClick(7))
+b7.grid(row=2, column=0)
+
+b8 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="8", bd=6, command=lambda: btnClick(8))
+b8.grid(row=2, column=1)
+
+b9 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="9", bd=6, command=lambda: btnClick(9))
+b9.grid(row=2, column=2)
+
+bAddition = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='white', bg='orange', text="+", bd=4,
+                   command=lambda: btnClick('+'))
+bAddition.grid(row=2, column=3)
+
+# 4 5 6  *
+
+b4 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="4", bd=6, command=lambda: btnClick(4))
+b4.grid(row=3, column=0)
+
+b5 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="5", bd=6, command=lambda: btnClick(5))
+b5.grid(row=3, column=1)
+
+b6 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="6", bd=6, command=lambda: btnClick(6))
+b6.grid(row=3, column=2)
+
+bMultiplication = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='white', bg='orange', text="*", bd=6,
+                         command=lambda: btnClick("*"))
+bMultiplication.grid(row=3, column=3)
+
+# 1 2 3 /
+
+b1 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="1", bd=6, command=lambda: btnClick(1))
+b1.grid(row=4, column=0)
+
+b2 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="2", bd=6, command=lambda: btnClick(2))
+b2.grid(row=4, column=1)
+
+b3 = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg='black', text="3", bd=6, command=lambda: btnClick(3))
+b3.grid(row=4, column=2)
+
+bDivide = Button(root, padx=10, pady=10, font=('arial', 18, 'bold'), fg="white", bg='orange', text="/", bd=6,
+                 command=lambda: btnClick('/'))
+bDivide.grid(row=4, column=3)
+
+# Equal sign
+
+bEqual = Button(root, font=('times', 20, 'bold'), bd=6, height=1, width=20, fg="black", text="Calculate",
+                command=btnEqual)
+bEqual.grid(columnspan=4)
+
+root.mainloop()
