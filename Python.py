4b0a92d9a9ccce87df1e4dd22bed8d7e7adeb00e
Sarfraz AHd <44200619+SarfrazAHd@users.noreply.github.com>
2019-04-28 11:49:02 +0530

    Delete Python.py

--- Python.py
+++
@@ -1,9 +0,0 @@
-print("Hello Users!!")
-
-print("this is the first step for me with github")
-
-
-#how to Create a function name in python:
-
-#def functionname(arguments):
-  #your code will place here inside of this function.
